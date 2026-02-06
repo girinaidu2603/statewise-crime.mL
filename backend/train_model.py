401f602a0732d162b70fa76a8bbd739a17ddc8bf
girinaidu2603 <girinaidu8688@gmail.com>
2026-02-07 01:51:53 +0530

    Update train_model.py

--- backend/train_model.py
+++ backend/train_model.py
@@ -1,1 +1,17 @@
-print('training model')
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+import os
+
+df = pd.read_csv("data/crime_data.csv")
+
+X = df[['Murder','Rape','Theft','Robbery','Cyber_Crime']]
+y = df['Total_Crime']
+
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X, y)
+
+os.makedirs("model", exist_ok=True)
+joblib.dump(model, "model/crime_model.pkl")
+
+print("Model trained successfully")
