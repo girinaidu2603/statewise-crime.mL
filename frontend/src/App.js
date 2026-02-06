ccbc6e319e17b7a0c0bf4b0b8ac2c24d9abe4879
girinaidu2603 <girinaidu8688@gmail.com>
2026-02-07 01:55:26 +0530

    Add App component with title and description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>State-wise Crime Analysis</h1>
+      <p>Machine Learning & Data Visualization for Public Safety</p>
+    </div>
+  );
+}
 
+export default App;
